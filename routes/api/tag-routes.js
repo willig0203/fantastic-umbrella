6e30b158eaa9c146dc480964100f8d9fa0c8b764
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-08-12 03:45:28 -0500

    tag routes working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -16,19 +16,77 @@ router.get("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  // and include its associated Product data
+  Tag.findAll({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+      },
+    ],
+  })
+    .then((dbTagData) => res.json(dbTagData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post("/", (req, res) => {
   // create a new tag
+  // TODO: check if it exists
+  //body
+  // {
+  //   "tag_name":"Pink"
+  // }
+  Tag.create({
+    tag_name: req.body.tag_name,
+  })
+    .then((dbTagData) => {
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
+  // {
+  // 	"tag_name":"Pink"
+  // }
+  Tag.update(
+    {
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: { id: req.params.id },
+    }
+  )
+    .then((dbTagData) => {
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: { id: req.params.id },
+  })
+    .then((dbTagData) => {
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
